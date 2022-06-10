aff1da46b4799d9e11fb215fa12dbae24f0bf68e
daichinagano <d.nagano1002@gmail.com>
2022-06-10 09:41:52 +0900

    :art: app-bar/cardコンポーネント追加

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <v-btn>button</v-btn>
-    <v-icon>mdi-alert-circle</v-icon>
-  </div>
+  <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>Nuxt3-Blog-App</v-app-bar-title>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+    <v-main>
+      <nuxt-page></nuxt-page>
+    </v-main>
+  </v-app>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-main>
+      <v-container fluid>
+        <v-row dense>
+          <v-col v-for="n in 4" :key="n" cols="8">
+            <div v-if="n === 1">
+              <v-card
+                :title="`Content ${n}`"
+                :subtitle="`Subtitle for Content ${n}`"
+                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
+                height="250"
+                elevation="2"
+              ></v-card>
+            </div>
+            <div v-else>
+              <v-card
+                :title="`Content ${n}`"
+                :subtitle="`Subtitle for Content ${n}`"
+                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
+                elevation="2"
+              ></v-card>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+// const ready = ref(false);
+</script>
