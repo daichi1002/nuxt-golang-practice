03cd886ff1796acc1a59195ffe61bde6cb521a7a
daichinagano <d.nagano1002@gmail.com>
2022-06-14 09:41:24 +0900

    :sparkles: backendにてgraphqlサーバー立ち上げ(Go)

---
+++ backend/graph/schema.resolvers.go
@@ -0,0 +1,29 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"backend/graph/generated"
+	"backend/graph/model"
+	"context"
+	"fmt"
+)
+
+func (r *queryResolver) Article(ctx context.Context, id int) (*model.Article, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+
+type mutationResolver struct{ *Resolver }
