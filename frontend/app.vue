45cf2f6b5ea385bfec20f0f9417a970fd12433a0
daichinagano <d.nagano1002@gmail.com>
2022-06-29 09:12:03 +0900

    :sparkles: リンク作成

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,21 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
   <v-app style="background-color: #f5f5f5">
-    <v-app-bar color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-app-bar-title>Nuxt3-Blog-App</v-app-bar-title>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <nuxt-link to="/articles/new">
-        <v-btn icon>
-          <v-icon>mdi-pencil-plus-outline</v-icon>
-        </v-btn>
-      </nuxt-link>
-      <v-btn icon="mdi-dots-vertical"></v-btn>
-    </v-app-bar>
+    <NuxtLayout />
     <v-main>
-      <nuxt-page></nuxt-page>
+      <NuxtPage />
     </v-main>
   </v-app>
 </template>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+definePageMeta({
+  layout: false,
+});
+
+const router = useRouter();
+
+const changeHomePage = () => {
+  router.push("/");
+};
+// const changeNewPage = () => {
+//   router.push("/articles/new");
+// };
+</script>
+<template>
+  <div>
+    <v-app-bar color="primary">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title> <a href="/">Nuxt3-Blog-App</a></v-app-bar-title>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <a href="/articles/new">
+        <v-btn icon>
+          <v-icon>mdi-pencil-plus-outline</v-icon>
+        </v-btn>
+      </a>
+
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<style type="text/css">
+a {
+  text-decoration: none;
+  color: #ffffff;
+}
+</style>
