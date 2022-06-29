e7d19b2641bdb9a3a7ec0ed7fc9638b2a5967eaf
daichinagano <d.nagano1002@gmail.com>
2022-06-29 09:52:39 +0900

    :art: wip:入力フォーム作成

--- frontend/pages/articles/new.vue
+++ frontend/pages/articles/new.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>test</div>
+  <v-main>
+    <v-form v-model="valid">
+      <v-container>
+        <v-text-field
+          v-model="firstname"
+          :rules="nameRules"
+          :counter="10"
+          label="Title"
+          color="purple lighten-3"
+          required
+        ></v-text-field>
+
+        <v-textarea
+          label="Content"
+          value=""
+          rows="16"
+          color="purple lighten-3"
+        ></v-textarea>
+      </v-container>
+    </v-form>
+  </v-main>
 </template>
