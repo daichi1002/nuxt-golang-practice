99a1c8acd9ba63ff99b9deef07ad76be1a999784
daichinagano <d.nagano1002@gmail.com>
2022-06-30 08:17:45 +0900

    :art: 入力フォーム作成

--- frontend/pages/articles/new.vue
+++ frontend/pages/articles/new.vue
@@ -1,23 +1,68 @@
+<script setup lang="ts">
+const valid = true;
+
+const titleRules = [
+  (v: string) => !!v || "Title is required",
+  (v: string) =>
+    (v && v.length <= 10) || "Title must be less than 10 characters",
+];
+
+type Article = {
+  title: string;
+  content: string;
+};
+
+const newArticle = reactive<Article>({
+  title: "",
+  content: "",
+});
+
+const save = () => console.log("save!");
+const reset = () => console.log("reset!");
+</script>
+
 <template>
   <v-main>
     <v-form v-model="valid">
       <v-container>
         <v-text-field
-          v-model="firstname"
-          :rules="nameRules"
+          v-model="newArticle.title"
+          :rules="titleRules"
           :counter="10"
           label="Title"
-          color="purple lighten-3"
+          color="primary"
           required
         ></v-text-field>
 
         <v-textarea
+          v-model="newArticle.content"
           label="Content"
-          value=""
-          rows="16"
-          color="purple lighten-3"
+          rows="14"
+          color="primary"
         ></v-textarea>
       </v-container>
     </v-form>
+    <v-row justify="center">
+      <v-btn
+        class="btn-interval"
+        color="primary"
+        variant="outlined"
+        @click="reset()"
+        >reset</v-btn
+      >
+      <v-btn
+        class="btn-interval"
+        color="primary"
+        variant="outlined"
+        @click="save()"
+        >save</v-btn
+      >
+    </v-row>
   </v-main>
 </template>
+
+<style>
+.btn-interval {
+  margin: 10px;
+}
+</style>
--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,10 +1,32 @@
 import "@mdi/font/css/materialdesignicons.css";
-import { createVuetify } from "vuetify";
+import { createVuetify, ThemeDefinition } from "vuetify";
 import * as components from "vuetify/components";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const myCustomLightTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+      background: "#FFFFFF",
+      surface: "#FFFFFF",
+      primary: "#4caf50",
+      "primary-darken-1": "#3700B3",
+      secondary: "#FFFFCC",
+      "secondary-darken-1": "#018786",
+      error: "#B00020",
+      info: "#2196F3",
+      success: "#4CAF50",
+      warning: "#FB8C00",
+    },
+  };
+
   const vuetify = createVuetify({
     components,
+    theme: {
+      defaultTheme: "myCustomLightTheme",
+      themes: {
+        myCustomLightTheme,
+      },
+    },
   });
 
   nuxtApp.vueApp.use(vuetify);
